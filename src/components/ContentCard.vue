97c9750dd1d51ba6a1a146b7a643d887aa8a1378
hnuikim-mn <cocacola2017@naver.com>
2022-10-10 01:12:07 +0900

    fix: prevent modal background 'scroll', limit description length

--- src/components/ContentCard.vue
+++ src/components/ContentCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card">
+  <div id="scrollTrue" class="card" >
     <div class="card-content">
         <div class="media">
             <div class="media-left">
@@ -8,7 +8,7 @@
                     <img class="is-rounded" v-else :src="contents.profile_img" :alt="profile_alt_img">
                 </figure>
             </div>
-            <div class="media-content" style="text-align: left">
+            <div class="media-content" >
                 <div class="title is-4">{{contents.title}}</div>
                 <div class="subtitle is-6">{{contents.author}}</div>
             </div>
@@ -21,9 +21,11 @@
         </figure>
     </div>
     <div class="card-content">
-        <div class="content">{{contents.reg_dt}}</div>
+        <div>{{contents.reg_dt}}</div>
+        <div>
         <div class="content">
-            {{contents.description}}
+            {{getDescription()}}
+        </div>
         </div>
     </div>
   </div>
@@ -31,7 +33,7 @@
 <script>
 export default {
     name: "ContentCard",
-    props: ['contents'],
+    props: ['contents', 'isMainList'],
     created(){
         console.log("CONTENTS", JSON.stringify(this.contents))
     },
@@ -39,7 +41,31 @@ export default {
         return {
             content_alt_img: "https://www.publicdomainpictures.net/pictures/280000/velka/not-found-image-15383864787lu.jpg",
             profile_alt_img: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80",
+            limitedDescription: null,
         }
     },
+    methods:{
+        getDescription(){ /* 메인노출인 카드: 100자 또는 2줄 제한 */
+            if(this.isMainList === true){
+                let desc = this.contents.description
+                const limit1lines = desc.indexOf("\n") 
+                const limit2lines = desc.indexOf("\n",limit1lines+1) 
+                console.log("LIMIT2LINES", limit2lines)
+                if(0<=limit2lines && limit2lines < 100){
+                    return desc.substring(0,limit2lines)+ "..."
+                }
+                    return desc.substring(0,100)+ "..."
+                // }
+            }
+            return this.contents.description
+        }
+    }
 }
 </script>
+<style>
+#scrollTrue{
+     overflow-y:scroll;
+    position:relative;
+    max-height: 800px;
+}
+</style>
