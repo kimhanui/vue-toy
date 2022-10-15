d0f0acb9d3e0a727d273c858b0995b46f6881dcf
hnuikim-mn <cocacola2017@naver.com>
2022-10-15 21:18:59 +0900

    update: skeleton on 'MainLayout', change click card area

--- src/components/ContentCard.vue
+++ src/components/ContentCard.vue
@@ -4,29 +4,45 @@
         <div class="media">
             <div class="media-left">
                 <figure class="image is-64x64">
-                    <img class="is-rounded" v-if="contents.profile_img == null || contents.profile_img == ''" :src="profile_alt_img" alt="profile">
-                    <img class="is-rounded" v-else :src="contents.profile_img" :alt="profile_alt_img">
+                    <template v-if="!loading">
+                        <img class="is-rounded" v-if="contents.profile_img == null || contents.profile_img == ''" :src="profile_alt_img" alt="profile">
+                        <img class="is-rounded" v-else :src="contents.profile_img" :alt="profile_alt_img">
+                    </template>
+                    <b-skeleton circle width="64px" height="64px" :active="loading"></b-skeleton>
                 </figure>
             </div>
             <div class="media-content" >
-                <div class="title is-4">{{contents.title}}</div>
-                <div class="subtitle is-6">{{contents.author}}</div>
+                <template v-if="!loading">
+                    <div class="title is-4">{{contents.title}}</div>
+                    <div class="subtitle is-6">{{contents.author}}</div>
+                </template>
+                <b-skeleton size="is-large"  width="80%" :active="loading"></b-skeleton>
+                <b-skeleton width="30%" :active="loading"></b-skeleton>
             </div>
         </div>
     </div>
-    <div class="card-image">
-        <figure class="image is-4by3">
-            <img v-if="contents.content_img == null || contents.content_img == ''" :src="content_alt_img" alt="content">
-            <img v-else :src="contents.content_img" alt="content">
+    <div class="card-image" @click="$emit('clickCard')">
+        <template v-if="!loading">
+            <figure class="image is-4by3">
+                <img v-if="contents.content_img == null || contents.content_img == ''" :src="content_alt_img" alt="content">
+                <img v-else :src="contents.content_img" alt="content">            
+            </figure>
+        </template>
+        <figure class="image">
+            <b-skeleton height="400px" :active="loading"></b-skeleton>
         </figure>
     </div>
-    <div class="card-content">
-        <div>{{contents.reg_dt}}</div>
-        <div>
-        <div class="content">
-            {{getDescription()}}
-        </div>
-        </div>
+    <div class="card-content" @click="$emit('clickCard')">
+        <template v-if="!loading">
+            <div>{{contents.reg_dt}}</div>
+            <div>
+            <div class="content">
+                {{getDescription()}}
+            </div>
+            </div>
+        </template>
+        <b-skeleton width="30%" :active="loading"></b-skeleton>
+        <b-skeleton width="80%" :active="loading" :count=3 ></b-skeleton>
     </div>
   </div>
 </template>
@@ -37,8 +53,14 @@ export default {
     created(){
         console.log("CONTENTS", JSON.stringify(this.contents))
     },
+    mounted(){
+        setInterval(() => {
+            this.loading = false
+        }, 2*1000);
+    },
     data(){
         return {
+            loading: true,
             content_alt_img: "https://www.publicdomainpictures.net/pictures/280000/velka/not-found-image-15383864787lu.jpg",
             profile_alt_img: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80",
             limitedDescription: null,
@@ -50,12 +72,10 @@ export default {
                 let desc = this.contents.description
                 const limit1lines = desc.indexOf("\n") 
                 const limit2lines = desc.indexOf("\n",limit1lines+1) 
-                console.log("LIMIT2LINES", limit2lines)
                 if(0<=limit2lines && limit2lines < 100){
                     return desc.substring(0,limit2lines)+ "..."
                 }
-                    return desc.substring(0,100)+ "..."
-                // }
+                return desc.substring(0,100)+ "..."
             }
             return this.contents.description
         }
@@ -64,7 +84,7 @@ export default {
 </script>
 <style>
 #scrollTrue{
-     overflow-y:scroll;
+    overflow-y:scroll;
     position:relative;
     max-height: 800px;
 }
