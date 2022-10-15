9fe508ee85ef9b7c7cf319224f6e93b7014f06e9
hnuikim-mn <cocacola2017@naver.com>
2022-10-15 22:46:06 +0900

    update: advance 'skeleton' by 'IntersectionObserver'

--- src/components/ContentCard.vue
+++ src/components/ContentCard.vue
@@ -1,48 +1,40 @@
 <template>
-  <div id="scrollTrue" class="card" >
+  <div class="card scrollTrue" :id="`card${contents.content_sq}`">
     <div class="card-content">
         <div class="media">
             <div class="media-left">
                 <figure class="image is-64x64">
-                    <template v-if="!loading">
+                    <template v-if="intersected">
                         <img class="is-rounded" v-if="contents.profile_img == null || contents.profile_img == ''" :src="profile_alt_img" alt="profile">
                         <img class="is-rounded" v-else :src="contents.profile_img" :alt="profile_alt_img">
                     </template>
-                    <b-skeleton circle width="64px" height="64px" :active="loading"></b-skeleton>
+                    <b-skeleton circle width="64px" height="64px" :active="!intersected"></b-skeleton>
                 </figure>
             </div>
             <div class="media-content" >
-                <template v-if="!loading">
                     <div class="title is-4">{{contents.title}}</div>
                     <div class="subtitle is-6">{{contents.author}}</div>
-                </template>
-                <b-skeleton size="is-large"  width="80%" :active="loading"></b-skeleton>
-                <b-skeleton width="30%" :active="loading"></b-skeleton>
             </div>
         </div>
     </div>
     <div class="card-image" @click="$emit('clickCard')">
-        <template v-if="!loading">
+        <template v-if="intersected">
             <figure class="image is-4by3">
                 <img v-if="contents.content_img == null || contents.content_img == ''" :src="content_alt_img" alt="content">
                 <img v-else :src="contents.content_img" alt="content">            
             </figure>
         </template>
         <figure class="image">
-            <b-skeleton height="400px" :active="loading"></b-skeleton>
+            <b-skeleton height="400px" :active="!intersected"></b-skeleton>
         </figure>
     </div>
     <div class="card-content" @click="$emit('clickCard')">
-        <template v-if="!loading">
-            <div>{{contents.reg_dt}}</div>
-            <div>
-            <div class="content">
-                {{getDescription()}}
-            </div>
-            </div>
-        </template>
-        <b-skeleton width="30%" :active="loading"></b-skeleton>
-        <b-skeleton width="80%" :active="loading" :count=3 ></b-skeleton>
+        <div>{{contents.reg_dt}}</div>
+        <div>
+        <div class="content">
+            {{getDescription()}}
+        </div>
+        </div>
     </div>
   </div>
 </template>
@@ -51,19 +43,21 @@ export default {
     name: "ContentCard",
     props: ['contents', 'isMainList'],
     created(){
-        console.log("CONTENTS", JSON.stringify(this.contents))
+        console.log("CONTENTS", (this.contents.title))
     },
     mounted(){
-        setInterval(() => {
-            this.loading = false
-        }, 2*1000);
+        this.executeIntersectionAPI()
+    },
+    destroyed(){
+        this.observer.disconnect()
     },
     data(){
         return {
-            loading: true,
+            intersected: false,
             content_alt_img: "https://www.publicdomainpictures.net/pictures/280000/velka/not-found-image-15383864787lu.jpg",
             profile_alt_img: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80",
             limitedDescription: null,
+            observer: null
         }
     },
     methods:{
@@ -78,12 +72,34 @@ export default {
                 return desc.substring(0,100)+ "..."
             }
             return this.contents.description
-        }
+        },
+        executeIntersectionAPI(){
+            const observerOption = {
+                rootMargin: '0px 0px 0px 0px',
+                threshold: 0.5,
+            };
+
+            let callback = (entries, observer) => {
+                let entry = entries[0]
+                console.log("FOR EACH", entry, entry.isIntersecting)
+
+                if (entry.isIntersecting) { // 뷰포트와 해당 요소가 threshold만큼 교차됐을 때 
+                    console.log("INTERSECT", true)
+                    this.intersected = true      
+                    observer.unobserve(entry.target)    // entry.target에 대해 관찰 종료
+                }
+            };
+                
+            this.observer = new IntersectionObserver(callback, observerOption);
+            
+            let elems = document.querySelector('#card'+this.contents.content_sq)
+            this.observer.observe(elems)
+        },
     }
 }
 </script>
 <style>
-#scrollTrue{
+.scrollTrue{
     overflow-y:scroll;
     position:relative;
     max-height: 800px;
--- src/components/MainLayout.vue
+++ src/components/MainLayout.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container is-max-desktop" style="width: 700px">
     <div v-if="contents != null">
-      <div id="content-list" v-for="(item, index) in contents" :key="index">
+      <div v-for="(item, index) in contents" :key="index">
         <content-card style="overflow:hidden" :contents="item" :isMainList="true" @clickCard="onClickCard(index)"/>
       </div>
     </div>
@@ -39,7 +39,6 @@ export default {
   data() {
     return {
       contents: null,
-      loading: true,
       selectedIndex: null,
       modals: {
         selectedCard: false,
@@ -53,37 +52,15 @@ export default {
     getContents() {
       // TODO : change to api call
       this.contents = card_list.contents;
+      this.executeIntersectionAPI()
     },
     onClickCard(index) {
       console.log("CLICK");
       this.selectedIndex = index;
       this.openModal('selectedCard')
     },
-    executeIntersectionAPI(){
-      const observerOption = {
-        rootMargin: '0px 0px 0px 0px',
-        threshold: 0.2,
-      };
+    infiniteScroll(){
 
-      let callback = (entries, observer) => {
-        entries.forEach((entry, index) => {
-            console.log("FOR EACH")
-            if (entry.isIntersecting) { // 뷰포트와 해당 요소가 threshold(0.2 =20%만큼 교차)만큼 교차됐을 때 
-                console.log("INTERSECT", true)
-                entry.target.src = this.contents[index].content_img
-                observer.unobserve(entry.target)    // entry.target에 대해 관찰 종료
-            }
-        });
-      };
-        
-      let observer = new IntersectionObserver(callback, observerOption);
-      
-      /**
-       * TODO - `ContentCard`를 어느 시점에 observe할 수 있는가?
-       * (api로 한번에 리스트 받아오는 동안 해당 객체는 모두 스켈레톤 처리함. 로드된 이후엔 굳이 Observer를 사용할 이유가 있나?)
-       */
-      let elems = document.querySelectorAll('content-card')
-      elems.forEach(el => {observer.observe(el)})
     },
   },
 };
