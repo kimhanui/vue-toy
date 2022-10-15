1d14990555652ce2cc6cb74c83408eff176500df
hnuikim-mn <cocacola2017@naver.com>
2022-10-15 23:31:17 +0900

    create: 'ScrollObserver' for infinite scroll

---
+++ src/components/ScrollObserver.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="observer"/>
+</template>
+<script>
+export default {
+    name: 'ScrollObserver',
+    data() {
+        return{
+            observer: null,
+        }
+    },
+    mounted() {
+        const options = this.options || {};
+        this.observer = new IntersectionObserver(([entry]) => {
+        if (entry && entry.isIntersecting) {
+            this.$emit("intersect");
+        }
+        }, options);
+
+        this.observer.observe(this.$el);
+    },
+    destroyed() {
+        this.observer.disconnect();
+    },
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ import AddCard from '../components/AddCard.vue'
 import CardComments from '../components/CardComments.vue'
 import ContentCard from '../components/ContentCard.vue'
 import LayoutHeader from '../components/LayoutHeader.vue'
+import ScrollObserver from '../components/ScrollObserver.vue'
 
 Vue.use(VueRouter)
 Vue.component(LayoutHeader.name, LayoutHeader)
@@ -15,6 +16,7 @@ Vue.component(ContentCard.name, ContentCard)
 Vue.component(CardComments.name, CardComments)
 Vue.component(MyStatistics.name, MyStatistics)
 Vue.component(MyPagination.name, MyPagination)
+Vue.component(ScrollObserver.name, ScrollObserver)
 
 export const router = new VueRouter({
     mode: 'history', // default는 hash 모드이다. history 모드를 적용하지 않으면 localhost:8080/#/ 이렇게 표시된다.
